02a5d21d529a524e24a9e313e7753c6773dd5417
BeemoLin <dteout@gmail.com>
2015-04-13 23:39:12 +0800

    power shutdown

--- runibike.sh
+++ runibike.sh
@@ -3,6 +3,6 @@ eval "$(rbenv init -)"
 
 xvfb-run ruby /home/beemolin/backup/ibike/ibike.rb
 
-sudo cp -r /home/beemolin/backup/ibike/ibike.json /var/lib/docker/aufs/mnt/544f95c2094cc2b2c320bb667a0514ee31eee569c0c195ea9c20ccd88f1375cf/
+sudo cp -r /home/beemolin/backup/ibike/ibike.json /var/lib/docker/aufs/mnt/a8ae26bcdce46d1a7d5a77fb687635debfc7b1fba49984aa706429926dfd8722/
 
-sudo docker exec -i 544f95c2094c cp /ibike.json /var/www/html/ibike.json
+sudo docker exec -i a8ae26bcdce4 cp /ibike.json /var/www/html/ibike.json
